6525d503dd586bd5448d3146d5fbf102527f4885
WesleyJ-128 <wesley.johnson.4096@gmail.com>
2020-09-24 18:47:27 -0400

    Commented Main.py

--- FLLMaster/Main.py
+++ FLLMaster/Main.py
@@ -1,40 +1,63 @@
 #!/usr/bin/env python3
 
+# Initialize loopIndex variable, used in displaySensor()
 loopIndex = 0
 
+# Import only the functions required for setup; 
+# MenuLib needs them run before importing other things
 from MenuLib import init, initthread
+# Used for printing to the VS Code output window
 from sys import stderr
 
+# Run the init function from MenuLib, config file name, 
+# used to instatiate the robot object
 init('robot.cfg')
+# Create the mission process used for running the current mission
 initthread()
+# Now that setup has finished, import everything else from MenuLib
 from MenuLib import *
+
+# Calibrate the gyro
 calibrate()
+# Calibrate the color sensor
 robot.reflectCal()
+# Initialization has finished, print status message to VS Code output window
 print("Finished Init", file=stderr)
 
+# Define the functions for button presses
 def left():
+    # Make sure there is no mission process running; if it is not, decrease count and update the display
+    # Otherwise, abort the current mission
     if not mission.is_alive():
         minusCount()
         display()
     else:
         abort()
 def right():
+    # Make sure there is no mission process running; if it is not, increase count and update the display
+    # Otherwise, abort the current mission
     if not mission.is_alive():
         addCount()
         display()
     else:
         abort()
 def down():
+    # Make sure there is no mission process running; if it is not, recalibrate the gyro
+    # Otherwise, abort the current mission
     if not mission.is_alive():
         calibrate()
     else:
         abort()
 def up():
+    # Make sure there is no mission process running; if it is not, recalibrate the color sensor
+    # Otherwise, abort the current mission
     if not mission.is_alive():
         robot.reflectCal()
     else:
         abort()
 def enter():
+    # Make sure there is no mission process running; if it is not, run the current mission, increase count, and update the display
+    # Otherwise, abort the current mission
     if not mission.is_alive():
         run()
         addCount()
@@ -42,8 +65,10 @@ def enter():
     else:
         abort()
 
+# Print to the VS Code window that the button functions have been defined
 print("Functions Defined", file=stderr)
 
+# Define a dictionary for calling the functions defined above based on the names for the buttons
 buttonMap = {
     'left': left,
     'right': right,
@@ -52,14 +77,22 @@ buttonMap = {
     'down': down
 }
 
+# Print to the VS Code window that the button dictionary has been defined
 print("Map Defined", file=stderr)
 
+# Start the infinite menu loop
 while True:
+    # This updates the status and process id of the mission process every loop cycle
     from MenuLib import mission
+    # Store the list of buttons currently pressed as buttonlist
     buttonlist = robot.btn.buttons_pressed
+    # Check if there are any buttons pressed; if there is, run the corresponding function
     if buttonlist:
         buttonMap[buttonlist[0]]()
+    # Increment loopIndex and reset to zero every hundreth loop
     loopIndex = (loopIndex + 1) % 100
+    # Make sure there is no mission running (to prevent resource conflicts), 
+    # and if there is none, display the sensor values and update the light color based on the gyro sensor
     if not mission.is_alive():
         checkDrift()
         displaySensor()
--- FLLMaster/Missions.py
+++ FLLMaster/Missions.py
@@ -3,13 +3,14 @@ if __name__ == "__main__":
     robot = Robot('robot.cfg')
 else:
     from MenuLib import robot
-# Write Mission Programs Here ----------------------
 
 # Mission Names should be prefaced with a##, where ## is two numbers.  These
 # numbers will not show up on the display, and tell the program what order the
 # missions should be in; a00 is first, followed by a01, and so on.  It is 
 # reccomended that spaces are left between the numbers (a00, a05, a10...), to
 # allow for future missions.
+
+# Write Mission Programs Here ----------------------
 
 def a00Star():
     # Drives a simple 4-pointed star, with arced sides
